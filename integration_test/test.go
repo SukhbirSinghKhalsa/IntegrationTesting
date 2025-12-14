76e0049bf5e51a9cd84697cac428b5d5e3d61fd8
Sukhbirsingh Khalsa <sukhbirkhalsait@gmail.com>
2025-12-14 23:18:59 +0530

    Fix syntax error in Terraform directory path

--- integration_test/test.go
+++ integration_test/test.go
@@ -11,7 +11,7 @@ import (
 func TestTerraformHelloWorldExample(t *testing.T) {
 	// retryable errors in terraform testing.
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		TerraformDir: "../terraform,
+		TerraformDir: "../terraform",
 	})
 
 	defer terraform.Destroy(t, terraformOptions)
